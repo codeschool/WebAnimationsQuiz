924a9272a89e26f936fe20688a0211c05f43b090
Drew Barontini <drewbarontini@gmail.com>
2015-07-23 06:11:56 -0400

    Disable submit button until answer is selected

--- source/javascripts/components/question.js
+++ source/javascripts/components/question.js
@@ -24,14 +24,23 @@ export default class extends React.Component {
     };
   }
 
+  _handleSelection( event ) {
+    this.setState( { selected: true } );
+  }
+
   _handleSubmit( event ) {
     this.props.submitHandler( event );
+    this.setState( { selected: false } );
   }
 
   render() {
     var answers = this.props.answers.map( ( answer, index ) => {
       return(
-        <li key={ index } className='answer'>{ answer }</li>
+        <li key={ index } className='answer'>
+          <a href='#' className='answer-link' onClick={ this._handleSelection.bind( this ) }>
+            { answer }
+          </a>
+        </li>
       );
     });
 
@@ -41,7 +50,7 @@ export default class extends React.Component {
         <ul className='has-answer'>
           { answers }
         </ul>
-        <button onClick={ this._handleSubmit.bind( this ) }>Next Question</button>
+        <button onClick={ this._handleSubmit.bind( this ) } disabled={ !this.state.selected }>Next Question</button>
       </div>
     );
   }
